1436f414e90d43856cfd8605ab09be0374bdb854
DavidBlaj <david_blaj2011@yahoo.com>
2025-09-14 19:13:49 +0200

    1.1 - api Ordner commit fix

--- src/api/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Expense';
-export * from './ExpenseCategory';
-export * from './ExpenseDto';
-export * from './Income';
-export * from './IncomeCategory';
-export * from './Saving';
